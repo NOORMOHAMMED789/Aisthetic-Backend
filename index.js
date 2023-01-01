d3f1988725ee5d0f7dcfedcb88560b5f80dc761a
Noor Mohammed <noormohammedknr2@gmail.com>
2023-01-01 17:35:00 +0530

    Added comments to the files

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//!Below API is used for post the data and store data in the MongoDB Altas(Day and Persons)
 app.use("/api/v1/user", require("./routes/userRouter"));
+
+//!Below API is used to get the data for the Items array list from the backend(Sambar, Coffee...)
 app.use("/api/v1/items", require("./routes/productRouter"));
 
+//!Below API is used for the CRUD operations on the data(GET,POST,PUT,DELETE) through RestAPI.
+app.use("/api/v1/order", require("./routes/orderRouter"));
+
+//!This is for the Database Connection.
 connection();
 
+//!This for the listening to the server at the port.
 app.listen(PORT, () => console.log("server is running on port", PORT));
 
 product_list();
---
+++ routes/orderRouter.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv").config();
+const Order = require("../modal/order");
+
+router.get("/", async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.json({
+      status: "Success",
+      orders,
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "Failed",
+      message: e.message,
+      orders: [],
+    });
+  }
+});
+
+//! This is for the read the data -- (CREATE -- POST Method)
+router.post("/", async (req, res) => {
+  try {
+    const orders = await Order.create(req.body);
+    res.json({
+      status: "Success",
+      orders,
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "Failed",
+      message: e.message,
+    });
+  }
+});
+
+//! This is for the read the data -- (UPDATE -- PUT Method)
+router.put("/:order_id", async (req, res) => {
+  try {
+    const data = await Order.findOne({ orderId: req.params.order_id });
+    res.json({
+      status: "Success",
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "Failed",
+      message: e.message,
+    });
+  }
+});
+
+//! This is for the read the data -- (DELETE -- DELETE Method)
+router.delete("/:id", async (req, res) => {
+  try {
+    const data = await Order.findOne({ id: req.params.id });
+  } catch (e) {
+    res.status(400).json({
+      status: "Failed",
+      message: e.message,
+    });
+  }
+});
+
+router.use("*", (req, res) => {
+  res.status(500).send("Invalid Url");
+});
+
+module.exports = router;
